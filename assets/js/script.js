3f95ff1cd66c5243a86b942b4135c7ed749ad5d7
Pedro Guarnieri <119369768+pdr606@users.noreply.github.com>
2024-05-20 20:58:33 -0300

    att

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,11 @@
 'use strict';
 
-
-
-/**
- * Add eventListener on multiple elements
- */
-
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
   }
 }
 
-
-
-/**
- * PRELOADER
- */
 
 const preloader = document.querySelector("[data-preloader]");
 
@@ -25,26 +14,12 @@ window.addEventListener("load", function () {
   document.body.classList.add("loaded");
 });
 
-
-
-/**
- * MOBILE NAV TOGGLE
- */
-
 const navbar = document.querySelector("[data-navbar]");
 const navToggler = document.querySelector("[data-nav-toggler]");
 
 const toggleNavbar = function () { navbar.classList.toggle("active"); }
 
 navToggler.addEventListener("click", toggleNavbar);
-
-
-
-/**
- * HEADER
- * 
- * active header when window scrolled to 50px
- */
 
 const header = document.querySelector("[data-header]");
 
